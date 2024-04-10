638af6e972fd934c623fc8476e65f4919ca7ce69
Mohd Masoom Ansari <masoomansari09875@gmail.com>
2024-04-10 13:37:27 +0530

    dom

---
+++ index.js
@@ -0,0 +1,21 @@
+let myDiv = document.querySelector('.myDiv');
+let para = document.querySelector('p');
+console.log(myDiv);
+console.log(para);
+
+para.style.color = 'blue';
+
+myDiv.style.backgroundColor = 'yellow'
+myDiv.style.height = '230px'
+myDiv.style.width = '330px'
+myDiv.style.paddingTop = '1px'
+myDiv.style.paddingLeft = '8px'
+myDiv.style.fontWeight = '18px'
+myDiv.style.margin = '20px'
+myDiv.style.fontSize = '20px'
+myDiv.style.fontWeight = 'bold'
+
+
+
+
+
